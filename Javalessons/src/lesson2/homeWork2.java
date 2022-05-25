b29f1289440689b74b96f3500b3cc0a459f2a8eb
LinnikAndrey <an.linnik@mail.ru>
2022-05-25 19:45:42 +0300

    lesson2

--- Javalessons/src/lesson2/homeWork2.java
+++ Javalessons/src/lesson2/homeWork2.java
@@ -2,6 +2,30 @@ package lesson2;
 
 public class homeWork2 {
     public static void main(String[] args) {
+        // a,b -between method, i-positiveornegative method
+        System.out.println(between(5,20));
+        positiveOrNegative(2);
+
+    }
+static boolean between(int a, int b) {
+    int s = a+ b;
+    if(s<=20 && s>=10){
+    return true;
+    }
+    else { return false;
 
     }
 }
+static void positiveOrNegative(int i) {
+
+
+    if (i >= 0) {
+
+        System.out.println("positive");
+    } else {
+        System.out.println("negative");
+    }
+}
+}
+
+
